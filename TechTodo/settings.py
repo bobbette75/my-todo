a5148d0c48b1090b6a74bd02c15aa35b036e2a6e
bobbette75 <olowookeresegun@yahoo.com>
2022-01-19 13:37:08 +0100

    skjdsd

--- TechTodo/settings.py
+++ TechTodo/settings.py
@@ -129,4 +129,4 @@ STATICFILES_DIRS =[os.path.join(BASE_DIR,'static')]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
